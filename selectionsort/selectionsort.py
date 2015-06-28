0ea07d084a1fa88f871b1f4a9129bfd7678e7e77
Rahul Nadella <rahulnadella@yahoo.com>
2015-06-28 15:31:54 -0400

    Updated SelectionSort algorithm

--- selectionsort/selectionsort.py
+++ selectionsort/selectionsort.py
@@ -1,15 +1,13 @@
 class SelectionSort(object):
 
-    def sort( aList ):
-      for i in range(len(aList)):
+    def sort(values):
+      for i in range(len(values)):
         least = i
-        for k in range(i + 1 , len(aList)):
-          if aList[k] < aList[least]:
+        for k in range(i + 1, len(values)):
+          if values[k] < values[least]:
             least = k
 
-        SelectionSort.swap( aList, least, i )
+        SelectionSort.swap(values, least, i)
 
     def swap( A, x, y ):
-      tmp = A[x]
-      A[x] = A[y]
-      A[y] = tmp
+        A[x], A[y] = A[y], A[x]
